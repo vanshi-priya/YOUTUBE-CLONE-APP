2c04b181f629a3c55299a75116be0a1ce4e06bc8
Vanshika Srivastava <vanshi27aug@gmail.com>
2024-11-03 15:08:07 +0530

    last commit

--- src/components/VideoContainer.js
+++ src/components/VideoContainer.js
@@ -1,7 +1,7 @@
-import React, { useEffect, useState } from 'react';
-import { YOUTUBE_VIDEOS_API } from '../utils/constants';
-import VideoCard, {AdVideoCard} from './VideoCard';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { YOUTUBE_VIDEOS_API } from "../utils/constants";
+import VideoCard, { AdVideoCard } from "./VideoCard";
+import { Link } from "react-router-dom";
 
 const VideoContainer = () => {
   const [videos, setVideos] = useState([]);
@@ -19,27 +19,12 @@ const VideoContainer = () => {
     const json = await data.json();
     console.log(json.items);
     setVideos(json.items);
-
   };
 
-  
   const [firstVideo, ...otherVideos] = videos;
 
   return (
-//     <div className="flex flex-wrap">
-//     {videos[0] && <AdVideoCard info={videos[0]} /> }
-//     {videos.map((video, index) =>( 
-//     <Link key={video.id} to={"/watch?v=" + video.id}>{index === 0 ? (
-//       <AdVideoCard info={video} />
-//     ) : (
-//       <VideoCard info={video} />
-//     )}</Link>
-//       ))}
-//     </div>
-//   )
-// };
-
-   <div className="flex flex-wrap">
+    <div className="flex flex-wrap">
       {/* Render the first video as an AdVideoCard if it exists */}
       {firstVideo && (
         <Link key={firstVideo.id} to={`/watch?v=${firstVideo.id}`}>
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,4 @@
-const GOOGLE_API_KEY = "AIzaSyArjVEJ9DxYFGOYIanY3LPcd9q022xq6Rw";
-// export const YOUTUBE_VIDEOS_API =
-// "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" + GOOGLE_API_KEY;
+const GOOGLE_API_KEY = process.env.REACT_APP_GOOGLE_API_KEY;
 
 export const YOUTUBE_VIDEOS_API =
   "https://www.googleapis.com/youtube/v3/videos?part=snippet,statistics&chart=mostPopular&regionCode=IN&maxResults=50&key=" +
